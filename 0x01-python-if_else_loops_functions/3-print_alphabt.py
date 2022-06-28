ad47174699ab247a4b467ab2c0a8cae314eb7dd1
layorcodes <99360584+layorcodes@users.noreply.github.com>
2022-06-29 00:25:53 +0800

    change

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-for lttr in range(97, 123):
-    if chr(lttr) != 'q' and chr(lttr) != 'e':
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
