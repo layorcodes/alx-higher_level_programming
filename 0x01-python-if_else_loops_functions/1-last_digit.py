a54115e528d9b722be3a11df075645cd6b552666
layorcodes <99360584+layorcodes@users.noreply.github.com>
2022-06-28 23:03:24 +0800

    last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
+ldigit = abs(number) % 10
 if number < 0:
-    last_digit = -last_digit
-if last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
-elif last_digit == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_digit))
+    ldigit = -ldigit
+print(f"Last digit of {number:d} is {ldigit:d} and is ", end="")
+if ldigit > 5:
+    print("greater than 5")
+elif ldigit == 0:
+    print("0")
 else:
-    print("Last digit of  {} is {} and is less than 6 and not 0".format(number, last_digit))
+    print("less than 6 and not 0")
